84d988a7d36a385be498be83e2d17a9843eeb1df
RodolfoSaldivar <33875215+RodolfoSaldivar@users.noreply.github.com>
2017-11-22 18:29:05 +0000

    checar auth en login

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -69,6 +69,8 @@ class UsersController extends AppController {
 	{
 		$this->layout = 'login';
 
+		if ($this->Session->check('Auth')) $this->redirect('/users/logout');
+
 	    if ($this->request->is('post'))
 	    {
 			$data = $this->request->data;
